2750516e19449b4056bb2ea267b02b0e5594d5ca
Dante <ajsantos.082@gmail.com>
2024-03-12 12:15:52 +0100

    Refactor: cambia color al liberar

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-function cambiaColor(event) {
-    const button = event.target
-    const nuevoColor = button.dataset.color
+function cambiaColor(color) {
     const square = document.getElementById("square")
-    square.className = `square ${nuevoColor}`
+    square.className = `square ${color}`
+}
+
+function cambiaColorAlSoltar(event) {
+    const color = event.target.dataset.color
+    cambiaColor(color)
 }
 
 const botones = document.querySelectorAll("[data-type=botones]")
 for (const button of botones) {
-    button.onclick = cambiaColor
+    button.onclick = cambiaColorAlSoltar
 }
