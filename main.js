0e377cee10abc28a6d48091f0fdbd60854c1debd
Dante <ajsantos.082@gmail.com>
2024-03-12 10:30:20 +0100

    Refactor: refactorizando funciones

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
-function cambiaColor(color1, color2) {
+function cambiaColor(event) {
+    const button = event.target
+    const primerColor = button.attributes.primerColor.value
+    const segundoColor = button.attributes.segundoColor.value
+
     const square = document.getElementById("square")
-    square.classList.remove(color1)
-    square.classList.add(color2)
-}
-
-function colorAzul(event) {
-    const button = event.target
-    const primerColor = button.attributes.primerColor.value
-    const segundoColor = button.attributes.segundoColor.value
-    cambiaColor(primerColor, segundoColor)
-}
-
-function colorRojo(event) {
-    const button = event.target
-    const primerColor = button.attributes.primerColor.value
-    const segundoColor = button.attributes.segundoColor.value
-    cambiaColor(primerColor, segundoColor)
+    square.classList.remove(primerColor)
+    square.classList.add(segundoColor)
 }
 
 const botonRojo = document.getElementById('botonRojo')
-botonRojo.onclick = colorRojo
+botonRojo.onclick = cambiaColor
 
 const botonAzul = document.getElementById('botonAzul')
-botonAzul.onclick = colorAzul
+botonAzul.onclick = cambiaColor
